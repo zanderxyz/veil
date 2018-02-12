2701b7ede3a6e8e6c9562021d2b31dc083652d5d
Zander Khan <git@zander.xyz>
2018-02-11 20:25:38 -0500

    Removed duplicate call to get_env

--- priv/templates/lib/web/emails/veil/login_email.ex
+++ priv/templates/lib/web/emails/veil/login_email.ex
@@ -5,19 +5,17 @@ defmodule <%= web_module %>.Veil.LoginEmail do
   Generates an email using the login template.
   """
   def generate(email, url) do
+    site = Application.get_env(:veil, :site_name)
+
     new()
     |> to(email)
     |> from(from_email())
-    |> subject("Welcome to #{Application.get_env(:veil, :site_name)}!")
-    |> render_body("login.html", %{url: url, site_name: site_name()})
+    |> subject("Welcome to #{site}!")
+    |> render_body("login.html", %{url: url, site_name: site})
   end
 
   defp from_email do
     {Application.get_env(:veil, :email_from_name),
      Application.get_env(:veil, :email_from_address)}
-  end
-
-  defp site_name do
-    Application.get_env(:veil, :site_name)
   end
 end
