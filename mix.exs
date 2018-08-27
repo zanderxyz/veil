21bf17fd774d078175f5a9d3e81401a17de272cb
Jay Jun <jayjun@MacBook.local>
2018-08-27 13:28:09 +0800

    Make Plug dependency explicit

--- mix.exs
+++ mix.exs
@@ -42,7 +42,8 @@ defmodule Veil.MixProject do
       {:ex_doc, ">= 0.0.0", only: :dev},
       {:swoosh, "~> 0.13"},
       {:phoenix_swoosh, "~> 0.2"},
-      {:quantum, "~> 2.2"}
+      {:quantum, "~> 2.2"},
+      {:plug, "~> 1.3"}
     ]
   end
 
